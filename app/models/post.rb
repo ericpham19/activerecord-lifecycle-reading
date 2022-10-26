280e324d1fd60dcf9757e7e98fa09001790d59a8
Duy Pham <ericpham19@outlook.com>
2022-10-26 21:38:44 +1100

    done

--- app/models/post.rb
+++ app/models/post.rb
@@ -3,6 +3,9 @@ class Post < ActiveRecord::Base
   belongs_to :author
   validate :is_title_case 
 
+  before_validation :make_title_case
+  before_save :give_warning_notification_about_post
+  # before_create :normalise_title
   private
 
   def is_title_case
@@ -11,6 +14,14 @@ class Post < ActiveRecord::Base
     end
   end
 
+  def give_warning_notification_about_post
+  
+  end
+
+  # def normalise_title
+  #   self.title= self.title.downcase.titleize
+  # end
+  def
   def make_title_case
     self.title = self.title.titlecase
   end
